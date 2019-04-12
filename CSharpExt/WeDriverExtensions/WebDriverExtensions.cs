2a47103fbb337e2559a0696c4b372c706c7bce67
asmashentsev <asmashentsev@esphere.ru>
2019-04-12 13:12:23 +0300

    driver extension template

--- CSharpExt/WeDriverExtensions/WebDriverExtensions.cs
+++ CSharpExt/WeDriverExtensions/WebDriverExtensions.cs
@@ -1,7 +1,52 @@
-﻿namespace CSharpExt.WeDriverExtensions
+﻿using System;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CSharpExt.WeDriverExtensions
 {
-    public class WebDriverExtensions
+    public static class WebDriverExtensions
     {
-        
+        /// <summary>
+        /// Получить стандартный waiter.
+        /// </summary>
+        /// <param name="driver">WebDriver object.</param>
+        /// <param name="seconds">Количество секунд.</param>
+        public static WebDriverWait GetWait(this IWebDriver driver, int seconds = 10)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+        }
+
+        /// <summary>
+        /// Find All Elements by selectors
+        /// </summary>
+        /// <param name="driver">Uses for any driver by IWebDriver interface</param>
+        /// <param name="by">By Selector</param>
+        /// <param name="timeoutInSeconds">Timeout in seconds</param>
+        /// <returns>Founded element collection</returns>
+        public static ReadOnlyCollection<IWebElement> FindElements(this IWebDriver driver, By by, int timeoutInSeconds = 0)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                wait.Until(drv => drv.FindElements(by).Count > 0);
+                return driver.FindElements(by);
+            }
+
+            return driver.FindElements(by);
+        }
+
+        /// <summary>
+        /// Extension method for explicit wait of web element without lazy initialiuzation*
+        /// </summary>
+        /// <param name="driver">Uses for any driver by IWebDriver interface</param>
+        /// <param name="by">By Selector</param>
+        /// <param name="timeoutInSeconds">Timeout in seconds</param>
+        /// <returns>Founded element</returns>
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(drv => drv.FindElement(by));
+        }
     }
 }
