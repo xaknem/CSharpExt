7ee4e90ae40788a98fbd83d827c9f20c20120441
asmashentsev <asmashentsev@esphere.ru>
2019-04-12 16:13:23 +0300

    Element Scroller

---
+++ CSharpExt/WeDriverExtensions/WebElementExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace CSharpExt.WeDriverExtensions
+{
+    public static class WebElementExtensions
+    {
+        /// <summary>
+        /// Scroll to element on the page.
+        /// Useful for angular elements that can be invisible outside the current browser position;
+        /// </summary>
+        /// <param name="webDriver">Uses for any driver by IWebDriver interface </param>
+        /// <param name="by">By Selector to find element </param>
+        /// <returns>Return founded element</returns>
+        public static IWebElement FindElementOnPage(this IWebDriver webDriver, By by)
+        {
+            RemoteWebElement element = (RemoteWebElement)webDriver.FindElement(by);
+            var hack = element.LocationOnScreenOnceScrolledIntoView;
+            return element;
+        }
+    }
+}
